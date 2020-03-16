9fff1a719e8f5a656d1b889ae1346bea4e729baa
Charlie Gleason <cgleasoniv@gmail.com>
2020-03-16 13:08:23 -0600

    install.sh: reorder commands, fix missing period

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/chriskempson/base16-shell.git $HOME/config/base16-shell
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-stow -t $HOME nvim
 stow -t $HOME zsh
+stow -t $HOME nvim
+git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
